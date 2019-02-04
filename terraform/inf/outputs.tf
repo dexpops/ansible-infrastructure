fc04b4aa39a5edf6a77dd4d19ae08359ed894e04
Karsten Kaj Jakobsen <kj@patientsky.com>
2019-02-04 15:52:16 +0100

    Moved inventory

--- terraform/inf/outputs.tf
+++
@@ -1,7 +0,0 @@
-// output "netbox" {
-//   value = "${digitalocean_droplet.netbox.name}: ${digitalocean_droplet.netbox.ipv4_address}"
-// }
-//
-// output "consul" {
-//   value = "${digitalocean_droplet.consul.name}: ${digitalocean_droplet.consul.ipv4_address}"
-// }
--- terraform/inf/ssh_keys.tf
+++
@@ -1,5 +0,0 @@
-# Create a new SSH key
-resource "digitalocean_ssh_key" "kj" {
-  name       = "cardno:000606334270"
-  public_key = "${var.ssh_key_public}"
-}
